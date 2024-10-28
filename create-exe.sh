6d76979d2d9828733f86f2985ce59915cde4a577
patrick <pa.trickjlp@gmail.com>
2024-10-28 09:38:11 +0100

    add release plan

--- create-exe.sh
+++
@@ -1,3 +0,0 @@
-node -e "require('fs').copyFileSync(process.execPath, 'devtools.exe')"
-node --experimental-sea-config sea-config.json
-npx postject devtools.exe NODE_SEA_BLOB sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2
